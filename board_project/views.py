ece24e910e7fb3e53d7d52ad9c94710fb3d4497f
youngchan-itall <michael.yc.cho@gmail.com>
2025-06-15 16:31:17 +0900

    feat: main 주소 변경

--- board_project/views.py
+++ board_project/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
-    """홈페이지"""
-    return render(request, "home.html")
+    """홈페이지 - estedu 웹사이트로 리다이렉트"""
+    return redirect("https://estedu.framer.website/")
